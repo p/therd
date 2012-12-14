f18e43c13d5993abccd7996ed70034ad81089f70
Oleg Pudeyev <oleg@bsdpower.com>
2012-12-14 03:30:45 -0500

    Add connect-assets

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.configure(function(){
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(require('connect-assets')());
 });
 
 app.configure('development', function(){
